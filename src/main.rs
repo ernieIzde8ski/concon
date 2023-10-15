2e0d5de332ee6c9f6ab5841175cd57de791b82c6
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2023-10-15 12:09:48 -0400

    Add cycling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod state;
 
 use state::State;
-use std::error;
+use std::{error, thread::sleep, time::Duration};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let fp = format!("{}/assets/sample.grid", env!("CARGO_MANIFEST_DIR"));
-    let grid = State::load(&fp)?;
-    println!("{grid}");
+    let mut state = State::load(&fp)?;
+    println!("{state}");
+    for _ in 0.. {
+        sleep(Duration::from_millis(200));
+        state.advance();
+        println!();
+        println!("{state}");
+    }
     Ok(())
 }
--- src/state.rs
+++ src/state.rs
@@ -60,19 +60,21 @@ impl TryFrom<String> for State {
     }
 }
 
+
 impl fmt::Display for State {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "╔{}╗", str::repeat("═", TOTAL_COLS))?;
         for row in self.grid {
+            write!(f, "║")?;
             for col in row {
                 match col {
-                    true => write!(f, "░")?,
-                    false => write!(f, "█")?,
+                    true => write!(f, "█")?,
+                    false => write!(f, " ")?,
                 };
             }
-            println!();
+            write!(f, "║\n")?;
         }
-
-        Ok(())
+        write!(f, "╚{}╝", str::repeat("═", TOTAL_COLS))
     }
 }
 
@@ -81,5 +83,73 @@ impl State {
         let mut fc = String::new();
         File::open(fp)?.read_to_string(&mut fc)?;
         Ok(Self::try_from(fc)?)
+    }
+}
+
+impl State {
+    /// Gets total number of live neighbors next to a grid point.
+    fn check_neighbors(&self, row: usize, col: usize) -> u8 {
+        let mut total_live_neighbors = 0;
+
+        for i in -1..=1 {
+            for j in -1..=1 {
+                if i == 0 && j == 0 {
+                    continue;
+                }
+
+                let r = match (i, row) {
+                    (-1, 0) => TOTAL_ROWS - 1,
+                    (-1, cur) => cur - 1,
+                    (1, mut cur) => {
+                        cur += 1;
+                        if cur == TOTAL_ROWS {
+                            0
+                        } else {
+                            cur
+                        }
+                    }
+                    (_, cur) => cur,
+                };
+
+                let c = match (j, col) {
+                    (-1, 0) => TOTAL_COLS - 1,
+                    (-1, cur) => cur - 1,
+                    (1, mut cur) => {
+                        cur += 1;
+                        if cur == TOTAL_COLS {
+                            0
+                        } else {
+                            cur
+                        }
+                    }
+                    (_, cur) => cur,
+                };
+
+                if self.grid[r][c] {
+                    total_live_neighbors += 1;
+                }
+            }
+        }
+        total_live_neighbors
+    }
+
+    /// Advances the grid by one cycle.
+    pub fn advance(&mut self) {
+        let mut next_grid = [[false; TOTAL_COLS]; TOTAL_ROWS];
+
+        for r in 0..TOTAL_ROWS {
+            for c in 0..TOTAL_COLS {
+                next_grid[r][c] = match (self.grid[r][c], self.check_neighbors(r, c)) {
+                    // Survival
+                    (true, 2 | 3) => true,
+                    // Birth
+                    (false, 3) => true,
+                    // No birth, or death by underpopulation or overpopulation
+                    (..) => false,
+                };
+            }
+        }
+
+        self.grid = next_grid;
     }
 }
